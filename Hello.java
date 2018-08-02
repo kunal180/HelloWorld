2aca0b09cfd6cc149eeded7f74ada3ca10d7fc96
kunal180 <40520902+kunal180@users.noreply.github.com>
2018-08-02 19:00:43 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello
     {
 	for(int i=0;i<10;i++)
       	{
-		System.out.println("Hello World.......aman."+(i+1));
+		System.out.println("Hello World.....aman."+(i+1));
 	}
       
    }
